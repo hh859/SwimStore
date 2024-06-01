45c0a8f41985b15547b6af26a1f0c844d121c531
hh859 H <hanna.hirenka@gmail.com>
2024-06-01 20:43:03 +0000

    another update before deploying

--- swimstore/settings.py
+++ swimstore/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hh859-swim-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['wim-store.herokuapp.com', 'localhost']
 
 
 # Application definition
